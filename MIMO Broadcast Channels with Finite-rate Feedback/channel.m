1009802e642ba1bc3e0974ca53965de647a0ed65
Zhou, Wentao <40170622+zhouwt612@users.noreply.github.com>
2021-03-25 17:32:12 +0900

    update210325

--- MIMO Broadcast Channels with Finite-rate Feedback/channel.m
+++
@@ -1,6 +0,0 @@
-function H = channel(K,M)
-% Channel function
-% K: the number of UE (assuming every UE with 1 antenna)
-% M: the number of transmit antennas
-
-H=(randn(K,M)+1i*randn(K,M))/sqrt(2);
