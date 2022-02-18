7cdd6c94d7ae2b6611c0e35a7aef9ca3aae7fd8c
Zhou, Wentao <zhouwt612@gmail.com>
2022-02-18 14:56:39 +0900

    update220218

---
+++ Antenna Combiners for Block-Diagonalization based Multi-User MIMO with Limited Feedback/METcombiner_forK.m
@@ -0,0 +1,9 @@
+function [H_MET,G_MET] = METcombiner_forK(H,L)
+[M,N,K] = size(H);
+H_MET = zeros(M,N,K);
+G_MET = zeros(N,N,K);
+for idx = 1:1:K
+    [Uk, ~, Vk] = svd(H(:,:,idx));
+    H_MET(:,:,idx) = Uk(:,1:L);
+    G_MET(:,:,idx) = Vk;
+end
---
+++ Antenna Combiners for Block-Diagonalization based Multi-User MIMO with Limited Feedback/SQBCcombiner_forK.m
@@ -0,0 +1,9 @@
+function G_SQBC = SQBCcombiner_forK(H,Hhat)
+[~,N,K] = size(H);
+G_SQBC = zeros(N,N,K);
+for idx1 = 1:1:K
+    [Uk,Sk,Vk] = svd(H(:,:,idx1));
+    Lambdak = Sk(1:N,1:N)^(-2);
+    Wk = Hhat(:,:,idx1)'*Uk(:,1:N);
+    G_SQBC(:,:,idx1) = Vk*Sk(1:N,1:N)^(-1)*Uk(:,1:N)'*Hhat(:,:,idx1)*Wk*Lambdak^(-1/2);
+end
